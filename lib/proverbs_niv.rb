3ceab8895c38d9d9257fe0fa7aadfd9ef94898f4
Kendra Kuivenhoven <kendrakuivenhoven@Kendras-MacBook-Pro.local>
2019-03-13 21:23:42 -0700

    (init): initial file push

---
+++ lib/proverbs_niv.rb
@@ -0,0 +1,71 @@
+require 'net/http'
+require 'json'
+
+class ProverbsNiv
+	@outerHash = Hash.new
+
+	def initialize(chapter, verses, stop_worded_verses, rec_nsw)
+		@chapter = chapter
+		@verses = verses
+		@stop_worded_verses = stop_worded_verses
+		@rec_nsw = rec_nsw
+	end
+
+	def get_chapter
+		return @chapter
+	end
+
+	def get_verses
+		return @verses
+	end
+
+	def get_sw_count
+		return @stop_worded_verses
+	end
+
+	def get_rec_nsw
+		return @rec_nsw
+	end
+
+	def self.init
+		uri = URI("http://bookofproverbs.herokuapp.com/api/v1/niv_bible")
+
+		http = Net::HTTP.new(uri.host, uri.port)
+		req = Net::HTTP::Get.new(uri.path)
+		resp = http.request(req)
+
+		data = JSON.parse(resp.body)
+		data.each do |key, value|
+			@localHash = Hash.new
+			@proverb = ProverbsNiv.new(key, value["verses"], value["stop_worded_verses"])
+			@localHash["chapter"] = @proverb.get_chapter
+			@localHash["verses"] = @proverb.get_verses
+			@localHash["rec_nsw"] = @proverb.get_rec_nsw
+			@localHash["sw_rec_count"] = @proverb.get_sw_count
+			@outerHash[@proverb.get_chapter] = @localHash
+		end
+		return "init complete"
+	end
+
+	def self.getAllChapters
+		return @outerHash.keys
+	end	
+
+	def self.getChapterContent(chapNum)
+		return @outerHash[chapNum.to_s]
+	end	
+
+	def self.getChapterVerses(chapNum)
+		return @outerHash[chapNum.to_s]["verses"]
+	end	
+
+	def self.getRecurrenceCountNoSw(chapNum)
+		return @outerHash[chapNum.to_s]["rec_nsw"]
+	end	
+
+	def self.getChapterSwVerses(chapNum)
+		return @outerHash[chapNum.to_s]["sw_rec_count"]
+	end	
+
+end
+
---
+++ proverbs_niv.gemspec
@@ -0,0 +1,12 @@
+Gem::Specification.new do |s|
+  s.name        = 'proverbs_niv'
+  s.version     = '1.0.1'
+  s.date        = '2019-03-13'
+  s.summary     = "A wrapper for the bookofproverbs.herokuapp.com API endpoint."
+  s.description = "Access to Proverbs NIV ('95) translation."
+  s.authors     = ["Kendra Kuivenhoven"]
+  s.email       = 'kkuivenhoven@mail.csuchico.edu'
+  s.files       = ["lib/proverbs_niv.rb"]
+  s.homepage    = 'http://rubygems.org/gems/proverbs_niv'
+  s.license     = 'MIT'
+end
